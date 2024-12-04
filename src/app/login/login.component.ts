bb3d8a390ab18e2933c4e78cd6ff66c64eb5c700
Laczik Tamás <tomlaczik@gmail.com>
2024-12-05 00:45:51 +0100

    Use material UI for forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm = this.formBuilder.group({
+    email: '',
+    password: '',
+  });
 
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
+
+  onLogin() {
+    this.http
+      .post('http://localhost:8000/api/sessions', this.loginForm.value, {
+        headers: { accept: 'application/json' },
+      })
+      .subscribe({
+        next: (response) => {
+          if ('token' in response && typeof response.token == 'string') {
+            localStorage.setItem('token', response.token);
+          }
+        },
+      });
+  }
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,12 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.scss'
+  styleUrl: './registration.component.scss',
 })
 export class RegistrationComponent {
+  registrationForm = this.formBuilder.group({
+    name: '',
+    email: '',
+    password: '',
+  });
 
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
+
+  onRegister() {
+    this.http
+      .post('http://localhost:8000/api/users', this.registrationForm.value)
+      .subscribe({
+        next: (response) => console.log(response),
+      });
+  }
 }
